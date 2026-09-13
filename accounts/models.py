fb6824524594c65747afa507ca42db6c7a8cefde
mbose360 <moatazbillahgahgah@gmail.com>
2026-09-13 12:57:31 +0200

    fix: allow blank usernames

--- accounts/models.py
+++ accounts/models.py
@@ -10,7 +10,7 @@ class User(AbstractUser):
  email = models.EmailField(unique=True)       
  role  = models.CharField( max_length=20 , choices=Role.choices)        
  phone_number = models.CharField( max_length=50 , blank= True)
-
+ username = models.CharField(max_length=20,blank=True)
  USERNAME_FIELD = "email"
  REQUIRED_FIELDS = []
 
